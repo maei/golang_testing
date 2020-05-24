aa3cba3ca73bd228ca5ff3e76279d8d7485ecbf1
maei <matthias.eiletz@aws-instit.de>
2020-05-24 22:57:00 +0200

    add blog_dto test scenario

---
+++ src/domain/blog_dto_test.go
@@ -0,0 +1,20 @@
+package domain
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestIsTitleUnique(t *testing.T) {
+	testService := NewBlogItemDomain()
+
+	// Fail Testcase
+	failString := ""
+	res := testService.IsTitleUnique(failString)
+	assert.Equal(t, false, res)
+
+	// Pass Testcase
+	validString := "vaild"
+	res1 := testService.IsTitleUnique(validString)
+	assert.Equal(t, true, res1)
+}
